de8db0c534368206c5b6b5f8ecbee7ac3c4b0ed4
Gil Felot <gfelot@gmail.com>
2016-10-25 22:01:07 +0200

    some little typo correction

--- config/FBauth.js
+++ config/FBauth.js
@@ -3,7 +3,7 @@ module.exports = {
   'facebookAuth' : {
     'clientID'      : '1777398592537368',
     'clientSecret'  : '9c2348696348ec0dabf3ab8e8f8b3b6f',
-    'callbackURL'   : 'http://localhost:8000/auth/facebook/callback'
-    'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
+    'callbackURL'   : 'http://localhost:8000/auth/facebook/callback',
+    'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
   }
 };
